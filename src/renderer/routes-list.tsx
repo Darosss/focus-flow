67b54229362dce816bed17b1d5a705f9e0e0ee8b
darosss <darosss12@proton.me>
2025-09-06 12:09:20 +0200

    feat(renderer/routes): add initial routes-list

---
+++ src/renderer/routes-list.tsx
@@ -0,0 +1,59 @@
+import { JSX } from "react";
+import {
+  LucideProps,
+  Timer,
+  History,
+  Activity,
+  BarChart3,
+  Settings,
+} from "lucide-react";
+import { DashboardView } from "./components/dashboard/dashboard";
+
+export type Route = {
+  id: string;
+  element: () => JSX.Element;
+  label: string;
+  icon: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+  description: string;
+};
+
+export const routesList: Route[] = [
+  {
+    id: "/",
+    element: DashboardView,
+    label: "Focus Dashboard",
+    icon: Timer,
+    description: "Live tracking & today's summary",
+  },
+  {
+    id: "activity",
+    element: () => <>activity</>,
+    label: "Timeline",
+    icon: Activity,
+    description: "Detailed activity timeline",
+  },
+  {
+    id: "analytics",
+    element: () => <>analytics</>,
+    label: "Analytics",
+    icon: BarChart3,
+    description: "Time patterns & insights",
+  },
+  {
+    id: "history",
+    element: () => <>history</>,
+    label: "History",
+    icon: History,
+    description: "Past days & trends",
+  },
+  {
+    id: "settings",
+
+    element: () => <>settings</>,
+    label: "Settings",
+    icon: Settings,
+    description: "Categories & preferences",
+  },
+];
