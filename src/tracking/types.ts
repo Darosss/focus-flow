00a8bddff2da00021715bc69908adf823973dcd9
darosss <darosss12@proton.me>
2025-09-07 17:40:15 +0200

    feat(types): add initial db types - add ActivbeSession - add SessionSnapshot - add AllTab - add SessionAllTabs

--- src/tracking/types.ts
+++ src/tracking/types.ts
@@ -10,3 +10,36 @@ export type DataJSON = {
     allWindows: ResultJSON[];
   };
 };
+
+export type ActiveSession = {
+  id: number;
+  appName: string;
+  platform: string;
+  startTime: Date;
+  endTime?: Date;
+};
+
+export type SessionSnapshot = {
+  id: number;
+  sessionId: number;
+  snapshotTime: Date;
+  title: string;
+  memoryUsage: number;
+};
+
+export type AllTab = {
+  id: number;
+  tabName: string;
+  firstSeen: Date;
+  lastSeen?: Date;
+};
+
+export type SessionAllTabs = {
+  tab_id: number;
+  sessionId: number;
+  tabName: string;
+  firstOpened: Date;
+  lastClosed?: Date;
+};
+
+export type TODO = any;
