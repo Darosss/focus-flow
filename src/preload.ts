e1095c5727578f6d28b2290f3155ac277ec7d869
darosss <darosss12@proton.me>
2025-09-24 13:22:17 +0200

    feat(ipc): add initial TrackerApi types - add getLastUsedApps - add getCurrentActivity

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,15 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from "electron";
+import { CurrentActivityData, TimeData } from "./tracking/types";
+
+contextBridge.exposeInMainWorld("trackerAPI", {
+  getLastUsedApps: () => ipcRenderer.invoke("get-last-used-apps"),
+  getCurrentActivity: () => ipcRenderer.invoke("get-current-activity"),
+});
+
+export type TrackerAPI = {
+  getLastUsedApps: () => Promise<TimeData[]>;
+  getCurrentActivity: () => Promise<CurrentActivityData | null>;
+};
