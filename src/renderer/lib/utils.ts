bf4000d41d00e83cef8f48880e819268aa887a73
darosss <darosss12@proton.me>
2025-09-26 23:45:15 +0200

    feat(renderer/utils): add formatMemoryUsage + formatDate

--- src/renderer/lib/utils.ts
+++ src/renderer/lib/utils.ts
@@ -18,3 +18,22 @@ export const formatTime = (seconds: number) => {
     return `${secs}s`;
   }
 };
+
+export const formatMemoryUsage = (bytes: number): string => {
+  if (bytes === 0) return "0 B";
+  const k = 1024;
+  const sizes = ["B", "KB", "MB", "GB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return (
+    Number.parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + " " + sizes[i]
+  );
+};
+
+export const formatDate = (date: Date): string => {
+  return new Date(date).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+};
